79799b36862e697a630b815068b679d7f6bfba08
Craig Gjerdingen <gjer0005@umn.edu>
2015-04-10 21:25:02 -0500

    Enhanced TrailReviews Create, Edit, Index including model attributes

--- MVCFundamentals/Models/TrailReview.cs
+++ MVCFundamentals/Models/TrailReview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace MVCFundamentals.Models
     public class TrailReview
     {
         public int Id { get; set; }
+        [Range(0,10)]
+        [Required]
         public int Rating { get; set; }
+        [Required]
+        [StringLength(1024)]
+        [Display(Name="Your Review")]
         public string Body { get; set; }
+
         public int TrailId { get; set; }
 
     }
