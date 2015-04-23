4f90ead9db23b2c7929a5ee1fe527c7d8e798a19
Craig Gjerdingen <gjer0005@umn.edu>
2015-04-23 14:48:06 -0500

    Replace asp.net Ajax.BeginForm with our own custom ajax script for more transparent enhancement and updating

--- MVCFundamentals/App_Start/BundleConfig.cs
+++ MVCFundamentals/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace MVCFundamentals
         {
             bundles.Add(new ScriptBundle("~/bundles/mytrailreviews").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery-validate*"));
+                "~/Scripts/jquery-validate*",
+                "~/Scripts/rmt*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
